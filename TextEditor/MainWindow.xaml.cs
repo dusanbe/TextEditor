55e16df985a6dfc81efe06f00b6a79c7166e0b0f
Dusan Biocanin <dusanbiocanin@hotmail.com>
2017-03-29 23:29:48 +0200

    Var1

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,93 @@ namespace TextEditor
 		public MainWindow()
 		{
 			InitializeComponent();
+			SetF1CommandBinding();
+		}
+
+		private void FileExit_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MouseEnterExitArea(object sender, MouseEventArgs e)
+		{
+			statBarText.Text = "Exit the Application";
+		}
+
+		private void MouseLeaveArea(object sender, MouseEventArgs e)
+		{
+			statBarText.Text = "Ready";
+		}
+
+		private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+		{
+			statBarText.Text = "Show Spelling Suggenstions";
+		}
+
+		private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+		{
+			string spellingHints = string.Empty;
+
+			SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+			if (error != null)
+			{
+				foreach (string s in error.Suggestions)
+				{
+					spellingHints += $"{s}\n";
+				}
+
+				lblSpellingHints.Content = spellingHints;
+				expanderSpelling.IsExpanded = true;
+			}
+		}
+
+		private void SetF1CommandBinding()
+		{
+			CommandBinding helpBinding = new CommandBinding(ApplicationCommands.Help);
+			helpBinding.CanExecute += CanHelpExecute;
+			helpBinding.Executed += HelpExecuted;
+			CommandBindings.Add(helpBinding);
+		}
+
+		private void CanHelpExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			MessageBox.Show("Look, it is not that difficult. Just type something!", "Help!");
+		}
+
+		private void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void SaveCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			var openDlg = new OpenFileDialog { Filter = "Text Files |*.txt" };
+
+			if (true == openDlg.ShowDialog())
+			{
+				string dataFromFile = File.ReadAllText(openDlg.FileName);
+				txtData.Text = dataFromFile;
+			}
+		}
+
+		private void SaveCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			var saveDlg = new SaveFileDialog { Filter = "Text Files |*.txt" };
+
+			if (true == saveDlg.ShowDialog())
+			{
+				File.WriteAllText(saveDlg.FileName, txtData.Text);
+			}
 		}
 	}
 }
